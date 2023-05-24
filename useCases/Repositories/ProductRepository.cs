aa0ee3490654a5151870b756d28a30196602c3a7
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-05-24 18:53:35 -0300

    Update ProductRepository.cs

--- useCases/Repositories/ProductRepository.cs
+++ useCases/Repositories/ProductRepository.cs
@@ -10,6 +10,8 @@ public class ProductRepository : IProductRepository
     new() {Id = 2, ProductName = "Mouse", ProductDescription = "Mouse da atualidade!", ProductPrice = 99.99, ProductImageURL = "https://m.media-amazon.com/images/I/712FFQs35IL._AC_SY679_.jpg" },
     new() {Id = 3, ProductName = "Computador", ProductDescription = "Computador para jogos!", ProductPrice = 5999.99, ProductImageURL = "https://images.kabum.com.br/produtos/fotos/sync_mirakl/150578/Computador-Gamer-AMD-10-Core-3-8Ghz-8GB-Radeon-R5-2GB-SSD-e-HD-2TB-Monitor-HDMI-LED-19-5-Kit-Gamer-Skill_1682012321_gg.jpg" }
   };
+  
+  
   public List<Product> GetAll()
   {
     return this._Products; 
